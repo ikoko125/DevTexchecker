b7bcdbca6774b8e460eaad6b4c6468bf163f6f97
ikoko125 <68625796+ikoko125@users.noreply.github.com>
2021-06-12 16:45:09 +0900

    release

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div id="box">
+      <div v-for="i in 100" :key="i" class="box" />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" src=".\checker.scss" />
